df09ff0d5f8c9f79b739b8a488269d3da7c73ad2
architmaurya <architmaurya7054@gmail.com>
2025-03-02 19:38:09 +0530

    complete the routes of the project

--- server/controllers/RatingAndReview.js
+++ server/controllers/RatingAndReview.js
@@ -1,5 +1,6 @@
 const RatingAndReview=require("../models/RatingAndReview")
 const Course=require("../models/Course");
+const { default: mongoose } = require("mongoose");
 
 
 //createRating 
@@ -58,7 +59,7 @@ exports.createRating=async(req,res)=>{
         console.log(error)
         return res.status(500).json({
             success:false,
-            message:"Rating and review not created Successfully",
+            message:error.message,
             
         })
     }
@@ -66,7 +67,76 @@ exports.createRating=async(req,res)=>{
 
 
 //getAveragerating
+exports.getAverageRating=async(req,res)=>{
+    try {
+        //get course id 
+        const courseId=req.body.courseId;
+        //calculate average rating
+        const result =await RatingAndReview.aggregate(
+            [
+                {$match:{
+                    course:new mongoose.Types.ObjectId(courseId),
+                        },
+                },
+                {
+                    $group:{
+                        _id:null,
+                        averageRating:{$avg:"$rating"},
+                    }
+                }
+            ]
+        )
+        //return rating
+        if(result.length>0){
+            return res.status(200).json({
+                success:true,
+                averageRating:result[0].averageRating,
+                
+            })
+        }
+        //id no rating review exist
+        return res.status(200).json({
+                success:true,
+                message:"Average rating is 0 ,no rating given till now ",
+                averageRating:0,
+           
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+            
+        })
+    }
+}
 
 
-
-//getAllrating
+//getAllratingandReview
+exports.getAllRating=async(req,res)=>{
+    try {
+        const allReviews=await RatingAndReview.find({})
+        .sort({ratind:"desc"})
+        .populate({
+            path:'user',
+            select:"firstName lastName email image",
+        })
+        .populate({
+            path:"course",
+            select:"coreseName"
+        })
+        .exec();
+        return res.status(200).json({
+            success:true,
+            message:"All reviews fetched successfully",
+            data:allReviews
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+            
+        })
+    }
+}
